a56190c7acb2c4be7aee487eea496bee0d397bcb
kev7862 <kevmatite7862@gmail.com>
2017-10-16 21:22:09 +0300

    Added 'org.apache.http.legacy' library that tests shadows, repositories and dependencies

--- app/src/main/java/kev7862/github/cuisinerestaurants/RestaurantsActivity.java
+++ app/src/main/java/kev7862/github/cuisinerestaurants/RestaurantsActivity.java
@@ -3,16 +3,22 @@ package kev7862.github.cuisinerestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+
+    TextView mLocationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        //Initializing my view
+        TextView mLocationView = (TextView)findViewById(R.id.restaurantsText);
 
        Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        mLocationView.setText("Here are all the restaurants near: " + location);
 
     }
 }
