75cdabfffa17aa7b77a528da2421fb855f334360
kev7862 <kevmatite7862@gmail.com>
2017-10-16 13:31:10 +0300

    Added Custom fonts to the main header in activity_main.xml

--- app/src/main/java/kev7862/github/cuisinerestaurants/MainActivity.java
+++ app/src/main/java/kev7862/github/cuisinerestaurants/MainActivity.java
@@ -1,13 +1,37 @@
 package kev7862.github.cuisinerestaurants;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mFindRestaurants;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mFindRestaurants = (Button)findViewById(R.id.findRestaurantsButton);
+        TextView textView = (TextView)findViewById(R.id.textView);
+
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/cartel.ttf");
+        textView.setTypeface(customFont);
+
+       mFindRestaurants.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               // Click Button code here.
+               Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
+           }
+       });
+
     }
 }
