83b98586ed56d3e05e9364721a6ff530cdb8c2db
kev7862 <kevmatite7862@gmail.com>
2017-11-03 11:58:06 +0300

    setting up our RestaurantsActivity class, making sure our restaurants Array passes

---
+++ app/src/androidTest/java/kev7862/github/cuisinerestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,8 @@
+package kev7862.github.cuisinerestaurants;
+
+/**
+ * Created by matite on 11/3/17.
+ */
+
+public class RestaurantsActivityInstrumentationTest {
+}
--- app/src/test/java/kev7862/github/cuisinerestaurants/RestaurantActivityTest.java
+++ app/src/test/java/kev7862/github/cuisinerestaurants/RestaurantActivityTest.java
@@ -1,20 +1,43 @@
 package kev7862.github.cuisinerestaurants;
 
 import android.os.Build;
+import android.widget.ListView;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by matite on 11/1/17.
  */
 
-// Adedd Annotations to enable our tests to run natively in the JVM instead of the android device
+// Added Annotations to enable our tests to run natively in the JVM instead of the android device
 @Config(manifest = "app/src/main/AndroidManifest.xml", constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricGradleTestRunner.class)
 
 public class RestaurantActivityTest {
+
+    private ListView mRestaurantListView;
+    private RestaurantsActivity activity;
+
+// We're Configuring the RestaurantActivity Class to know from where we're running the test from
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(RestaurantsActivity.class);
+        mRestaurantListView = (ListView) activity.findViewById(R.id.restaurant);
+    }
+
+    @Test
+    public void restaurantListViewPopulates(){
+        assertNotNull(mRestaurantListView.getAdapter());
+        assertEquals(mRestaurantListView.getAdapter().getCount(), 26);
+    }
 
 
 }
